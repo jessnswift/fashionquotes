ccc51fc87aa89c2f44757c6d9a2bf55967a66660
Jessica Van Meter <jessicavanmeter@jessicas-mbp.attlocal.net>
2017-11-09 21:01:30 -0600

    inital state of fashion quotes

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var quotes = [
+  'Always Play Dressup!',
+  'I like my money right where I can see it, hanging in my closet.',
+  'When I first moved to New York and I was totally broke, sometimes I would buy Vogue instead of dinner. I felt it fed me more.'
+];
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+
+// Get a hold of the button by its id
+var buttonEL = document.getElementById('loadQuote')
+// Add an event listener to the button
+buttonEL.addEventListener("click", printQuote, false);
+
+// Our event listener function for the button
+function printQuote() {
+    var quoteElements = document.getElementsByClassName('quote');
+    var quoteParagraph = quoteElements[0];
+    quoteParagraph.innerHTML = 'donkey';
+}
